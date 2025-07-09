166b7368c94092439e37c4afbc7d5efc5c77913c
thebyronc <thebyronc@gmail.com>
2025-07-09 11:59:11 -0700

    update intro

--- app/pages/Contact/Contact.tsx
+++ app/pages/Contact/Contact.tsx
@@ -1,13 +1,15 @@
-export function Contact() {
+import "./contact.css";
+
+export default function Contact() {
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
-      <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
+      <div className="flex min-h-0 flex-1 flex-col items-center gap-16">
         <header className="flex flex-col items-center gap-9">
           <div className="w-[500px] max-w-[100vw] p-4">
             <p>Contact Information</p>
           </div>
         </header>
-        <div className="max-w-[300px] w-full space-y-6 px-4"></div>
+        <div className="w-full max-w-[300px] space-y-6 px-4"></div>
       </div>
     </main>
   );
